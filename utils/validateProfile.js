1577e9be04abab071f95781b277ad17cbefd5adb
farazkhanfk7 <farazkhan138@gmail.com>
2023-04-08 21:34:45 +0530

    fix edit profile

--- utils/validateProfile.js
+++ utils/validateProfile.js
@@ -2,9 +2,9 @@ const joi = require("joi");
 
 const validateProfile = (profile) => {
     const schema = joi.object({
-        bio: joi.string(),
-        department: joi.string(),
-        location: joi.string(),
+        bio: joi.string().allow(''),
+        department: joi.string().allow(''),
+        location: joi.string().allow(''),
         interest: joi.array(),
     })
 
