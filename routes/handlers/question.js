aaf558b26c01e6847f207791a281696f223e83bf
farazkhanfk7 <farazkhan138@gmail.com>
2023-05-13 16:46:29 +0530

    fix upvote

--- routes/handlers/question.js
+++ routes/handlers/question.js
@@ -24,7 +24,7 @@ const upvoteQuestion = async (req, res, session) => {
   if (question.upvotes.includes(req.user.id)) {
     question.upvotes.pull(req.user.id);
     await question.save({ session });
-    const response = get_response_dict(200, 'Upvote Removed', null);
+    const response = get_response_dict(200, 'Upvote Removed',question);
     return res.status(201).json(response);
   }
 
@@ -68,7 +68,7 @@ const downvoteQuestion = async (req, res, session) => {
   if (question.downvotes.includes(req.user.id)) {
     question.downvotes.pull(req.user.id);
     await question.save({ session });
-    const response = get_response_dict(200, 'Downvote Removed', null);
+    const response = get_response_dict(200, 'Downvote Removed', question);
     return res.status(201).json(response);
   }
 
